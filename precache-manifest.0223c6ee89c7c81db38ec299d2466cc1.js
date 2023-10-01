75c3aed2390aa08f50be2697854f58fdf6ac2992
Brayan-Alexis <brayanzxd@gmail.com>
2023-09-30 18:02:45 -0700

    Updates

---
+++ precache-manifest.0223c6ee89c7c81db38ec299d2466cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c02f12badb6396b78e1",
+    "url": "/todo-nest/static/css/main.a7638c9e.chunk.css"
+  },
+  {
+    "revision": "1c02f12badb6396b78e1",
+    "url": "/todo-nest/static/js/main.b8a27b4c.chunk.js"
+  },
+  {
+    "revision": "eb88e64c9870a3bb7f6d",
+    "url": "/todo-nest/static/js/runtime~main.b52e8436.js"
+  },
+  {
+    "revision": "27e658d0dc6bc3f0c25b",
+    "url": "/todo-nest/static/js/2.b1e17e5c.chunk.js"
+  },
+  {
+    "revision": "48106c06b48b4259cbe1c101814f671a",
+    "url": "/todo-nest/index.html"
+  }
+];
